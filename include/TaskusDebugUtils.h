ad67b4dabe78537f3b2b708621862b9a1047bf3a
Luis Duarte <luis.duarte.2003.13@hotmail.com>
2022-08-16 20:31:57 +0100

    Fix the offsets because i've missplaced them lmao

--- include/TaskusDebugUtils.h
+++ include/TaskusDebugUtils.h
@@ -15,8 +15,8 @@ namespace Taskus{
     #define SVG_RADIUS_X 60
     #define SVG_RADIUS_Y 40
     #define SVG_TEXT_OFFSET 4 
-    #define SVG_OFFSET_BETWEEN_TASKS_X 100 //this offset is used when there are multiple dependantTasks 
-    #define SVG_OFFSET_BETWEEN_TASKS_Y 400
+    #define SVG_OFFSET_BETWEEN_TASKS_X 400 //this offset is used when there are multiple dependantTasks 
+    #define SVG_OFFSET_BETWEEN_TASKS_Y 200
     
     static std::function<std::string(Task*)>  _defaultTaskNameFunction = [](Task* task){static int i = 0; std::string s = "Task "+ std::to_string(i); i++; return s;};
 
